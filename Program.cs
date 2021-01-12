696874a39c699df7560fa154acec35910560cbc2
Felipe <felipe.rodrigues@talkandlive.com>
2021-01-12 19:30:20 -0300

    Adiciona arquivos do projeto base

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lista_de_Contatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
